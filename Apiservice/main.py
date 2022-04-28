8b212a462877fa7d427a7fe225ae869a7492db24
Andrii <kollider184@gmail.com>
2022-04-28 07:07:06 +0300

    Added sending image over json

--- Apiservice/main.py
+++ Apiservice/main.py
@@ -1,25 +1,67 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Run a Flask REST API exposing a YOLOv5s model
+"""
+
+import argparse
+import base64
+import io
+
+import torch
 import uvicorn
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, Depends, Request
+from PIL import Image
 
 app = FastAPI()
 
-
-def save_file(filename, data):
-	with open(filename, 'wb') as f:
-		f.write(data)
+DETECTION_URL = "/upload"
 
 
-@app.get("/")
-async def home():
-	return {"message": "Hello World"}
+async def parse_body(request: Request):
+	data: bytes = await request.body()
+	return data
 
 
-@app.post("/upload")
-async def image_process(file: UploadFile = File(...)):
-	contents = await file.read()
-	save_file(file.filename, contents)
-	return {"Filename": file.filename}
+def image_to_byte_array(in_image: Image) -> bytes:
+	imgByteArr = io.BytesIO()
+	in_image.save(imgByteArr, format=in_image.format)
+	byteIm = imgByteArr.getvalue()
+	return byteIm
+
+
+@app.post(DETECTION_URL)
+async def image_process(data: bytes = Depends(parse_body)):
+	image_bytes = data
+	image = Image.open(io.BytesIO(image_bytes))
+	results = model(image)
+	tdict = {}
+	i = 1
+	for img in results.imgs:
+		# tdict['spec']=img
+		print(results)
+		print(img)
+
+	results.render()
+
+	for img in results.imgs:
+		buffered = io.BytesIO()
+		img_base64 = Image.fromarray(img)
+		img_base64.save(buffered, format="JPEG")
+		img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')  # base64 encoded image with results
+		tdict[f'{i}'] = img_str
+	# bla = image_to_byte_array(Image.open('runs/de'))
+	# tdict['additional']=results.pandas().xyxy[0].to_json(orient="records")
+
+	return tdict  # results.pandas().xyxy[0].to_json(orient="records")
 
 
 if __name__ == '__main__':
-	uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
+	parser = argparse.ArgumentParser(description="Flask API exposing YOLOv5 model")
+	parser.add_argument("--port", default=8000, type=int, help="port number")
+	opt = parser.parse_args()
+
+	# Fix known issue urllib.error.HTTPError 403: rate limit exceeded https://github.com/ultralytics/yolov5/pull/7210
+	torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+
+	model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True, force_reload=False)
+	uvicorn.run(app, host="127.0.0.1", port=opt.port)  # debug=True causes Restarting with stat
--- client.py
+++ client.py
@@ -1,24 +1,37 @@
-"""import requests
-
-url = 'http://127.0.0.1:8000/upload'
-file = {'file': open('images/zidane.jpg', 'rb')}
-resp = requests.post(url=url, files=file)
-print(resp.json())
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
 """
+Perform test request
+"""
+import base64
 import io
+import pprint
+
+import requests
 from PIL import Image
 
-im = Image.open('images/zidane.jpg')
+DETECTION_URL = "http://127.0.0.1:8000/upload/"
+IMAGE = Image.open('images/zidane.jpg')
 
 
-def image_to_byte_array(image: Image) -> bytes:
-  imgByteArr = io.BytesIO()
-  image.save(imgByteArr, format=image.format)
-  byteIm = imgByteArr.getvalue()
-  return byteIm
+def image_to_byte_array(in_image: Image) -> bytes:
+	imgByteArr = io.BytesIO()
+	in_image.save(imgByteArr, format=in_image.format)
+	byteIm = imgByteArr.getvalue()
+	return byteIm
 
-bla = image_to_byte_array(im)
 
-image = Image.open(io.BytesIO(bla))
+bla = image_to_byte_array(IMAGE)
 
-image.show()
+# Read image
+
+
+response = requests.post(DETECTION_URL, data=bla).json()
+img_str = response['1']
+img_base64 = base64.b64decode(img_str)
+img_bio = io.BytesIO(img_base64)
+
+img = Image.open(img_bio)
+img_shape = img.size
+#print(type(response['spec']))
+
+# pprint.pprint(response)
